e6d66e02fea1712678517a86d079d20a5e6d0ba7
Eric Belongea <eric.belongea@gmail.com>
2023-12-12 18:19:19 -0700

    Can't create a viewing party without being logged in

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -5,12 +5,21 @@ class ViewingPartiesController < ApplicationController
   end 
   
   def create 
-    user = User.find(params[:user_id])
-    user.viewing_parties.create(viewing_party_params)
-    redirect_to "/users/#{params[:user_id]}"
+    if !current_user
+      require_current_user
+    else
+      user = User.find(params[:user_id])
+      user.viewing_parties.create(viewing_party_params)
+      redirect_to dashboard_path(current_user.id)
+    end
   end 
 
   private 
+
+  def require_current_user
+    flash[:error] = "You must be logged in or register an account in order to go to your dashboard"
+    redirect_to root_path
+  end
 
   def viewing_party_params 
     params.permit(:movie_id, :duration, :date, :time)
